7fd30be12c17598173d065a7e4eb5d5531be6042
khushbooz <avinash2014mca@gmail.com>
2023-09-13 08:18:17 +0530

    test

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
                 <a className="nav-link active" aria-current="page" href="#">Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
+                <a className="nav-link" href="#">About</a>
               </li>
             </ul>
             <form className="d-flex" role="search">
