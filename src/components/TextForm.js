e4a00bb7e3ab0cd634b3554063c54c43538c8cb2
khushbooz <avinash2014mca@gmail.com>
2023-09-16 08:53:05 +0530

    'texttospeech'

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,24 +1,41 @@
 import React, {useState} from 'react'
+import { useSpeechSynthesis } from "react-speech-kit";
 
 export default function TextForm(props) {
   const [text, setText] = useState('Please Enter');
+  const { speak } = useSpeechSynthesis();
+
   const handleClick = () => {
     console.log('handleClick' + text);
     let newTxt = text.toUpperCase();
     setText(newTxt);
     console.log(newTxt);
   }
+  const handleSpeachClick = () => {
+    speak({ text: text })
+  }
   const handleChange = (event) => {
     console.log('handleChange');
     setText(event.target.value);
   }
   return (
+    <>
     <div>
         <div className="mb-3">
-            <label forhtml="exampleFormControlTextarea1" className="form-label">{props.heading}</label>
-            <textarea className="form-control" value = {text} onChange={handleChange} id="exampleFormControlTextarea1" rows="8"></textarea>
-            <button className="btn btn-primary" onClick={handleClick} >Convert to Uppercase</button>
+            <label forhtml="exampleFormControlTextarea1" className="form-label mx-2">{props.heading}</label>
+            <textarea className="form-control mx-2" value = {text} onChange={handleChange} id="exampleFormControlTextarea1" rows="8"></textarea>
+            <button className="btn btn-primary mx-2" onClick={handleClick} >Convert to Uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleSpeachClick} >Speech</button>
         </div>
     </div>
+    <div className="container">
+      <h1> Summary</h1>
+      <p>
+      {text.split(' ').length} words and {text.length} characters
+      </p>
+
+    </div>    
+    </>
+
   )
 }
