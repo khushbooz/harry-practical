8048262150baad6aa5fed03918371ceae1fca8ce
khushbooz <avinash2014mca@gmail.com>
2023-09-19 11:50:11 +0530

    'reactrouter'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import "./index.css";
 import React, {useState} from 'react'
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
 import Alert from './components/Alert';
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
+// import * as ReactDOM from "react-dom/client";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [oldMode, setOldMode] = useState('dark');
@@ -52,12 +64,31 @@ function App() {
   return (
     // <div className="App">
       <>
-        <Navbar newModes = {newModes} oldMode = {oldMode} toggleMode = {toggleMode} />
-        <Alert alert = {alert} />
-        <div className="container">
-          <TextForm heading = 'My Heading' newModes = {newModes} showAlert = {showAlert} />   
-        </div>
-        <About />
+      {/* In react-router-dom v6, "Switch" is replaced by routes "Routes". You need to update the import from */}
+      {/* import { Switch, Route } from "react-router-dom";
+      to
+      import { Routes, Route } from 'react-router-dom';
+
+      <Route path="/" component={Home} />
+      to
+      <Route path='/' element={<Home/>} /> */}
+
+        <Router>
+          <Navbar newModes = {newModes} oldMode = {oldMode} toggleMode = {toggleMode} />
+          <Alert alert = {alert} />
+          <div className="container my-3">
+            {/* <TextForm heading = 'My Heading' newModes = {newModes} showAlert = {showAlert} /> */}
+            <Routes>
+              {/* /users --> Component 1
+                  /users/home --> Component 2 */}
+              <Route exact path="/about" element={<About />} >
+              </Route>
+              <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" newModes = {newModes}/>}>
+              </Route>
+            </Routes>
+          </div>
+          {/* <About /> */}
+        </Router>
       </>
 
       /* <div className="blank"></div>
